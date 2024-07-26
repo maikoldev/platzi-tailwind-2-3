4e527934ae98db8860f1300bd80fe3950ac82543
krikadev <krikadev@gmail.com>
2024-07-26 15:57:03 -0500

    Adding darkMode and Details

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: "selector",
   theme: {
-    colors: {
-      "brick-red": "#CC2D4A",
-      "light-grey": "#CCCCCC",
-      "picton-blue": "#61AEC9",
-      black: "#000000",
-      citrus: "#8FA206",
-      snow: "#FCFBFB",
-      whisper: "#e5e5e5",
-      white: "#FFFFFF",
-    },
     extend: {
-      fontFamily: {
-        sans: ["Montserrat", "sans-serif"],
-      },
       boxShadow: {
         card: "0px 3px 3px 0px rgba(0, 0, 0, 0.25)",
         input: "0px 2px 7px 0px rgba(0, 0, 0, 0.25)",
+      },
+      colors: {
+        "brick-red": "#CC2D4A",
+        "light-grey": "#CCCCCC",
+        "picton-blue": "#61AEC9",
+        citrus: "#8FA206",
+        snow: "#FCFBFB",
+        whisper: "#e5e5e5",
+      },
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
       },
     },
   },
